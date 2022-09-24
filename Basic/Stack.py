5ed2aacc786d98306d8acdea1945ea8ee238fdf6
maxwell <max870701@gmail.com>
2022-09-24 00:00:23 -0700

    Stack

---
+++ Basic/Stack.py
@@ -0,0 +1,36 @@
+from fcntl import I_SETCLTIME
+from multiprocessing.util import is_abstract_socket_namespace
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+    def push(self, element):
+        self.stack.append(element)
+    def pop(self):
+        return self.stack.pop()
+    def get_top(self):
+        if len(self.stack) > 0:
+            return self.stack[-1]
+        else:
+            return None
+    def is_empty(self):
+        return len(self.stack) == 0
+
+def brace_match(s):
+    match_dict = {'}':'{', ']':'[', ')':'('}
+    stack = Stack()
+    for ch in s:
+        if ch in {'(', '[', '{'}:
+            stack.push(ch)
+        else:
+            if stack.is_empty():
+                return False
+            elif stack.get_top() == match_dict[ch]:
+                stack.pop()
+            else: # stack.get_top() != match_dict[ch]
+                return False
+    if stack.is_empty():
+        return True
+    else:
+        return False
