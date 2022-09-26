27993bd21104499febd603bcafa96351cc69e7e0
maxwell <max870701@gmail.com>
2022-09-26 15:03:31 -0700

    27. Remove Element

---
+++ 27-RemoveElement.py
@@ -0,0 +1,73 @@
+from sys import last_value
+
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0
+        for n in nums:
+            if nums[i] == val:
+                nums.pop(i)
+                nums.append(-1)
+            else:
+                i += 1
+        return i
+
+    def removeElement2(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        writer = 0
+        reader = 0
+        while reader < len(nums):
+
+            if nums[reader] != val:
+                nums[writer] = nums[reader]
+                writer += 1
+
+            reader += 1
+
+        return writer
+
+    def removeElement3(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        writer = 0
+        reader = 0
+        while reader < len(nums):
+
+            if nums[reader] == val:
+                reader += 1
+                continue
+            else:
+                nums[writer] = nums[reader]
+                writer += 1
+
+            reader += 1
+
+        return writer
+
+    def removeElement4(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        i = 0 # The first pointer starts at 0 position
+        l = len(nums) # The second pointer starts at the last position
+        while i < l:
+            if nums[i] == val:
+                nums[i] = nums[l - 1] # swap
+                l -= 1
+            else:
+                i += 1
+        return l
--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # LeetCode Record in Python3
 ## Sort
 - 876 Middle of the linkded list
-> Skills: the fast and slow pointer
+> The fast and slow pointer
 - 21 Merge two sorted list
-> Skills: Recursion/ Empty linked list/ Iteration
+> Recursion/ Empty linked list(sorted and return its next attribute)/ Iteration
 - 148 Sort List
-> Skills: Merge Sort/ Middle of the linked list/ Merge two sorted list
+> Merge Sort/ Middle of the linked list/ Merge two sorted list
+- 56 Merge Intervals
+> python built-in function(sort, max, min)/ an initial list/ define merge rules(when and which values) 
+- 27 Remove Element
+> two pointer (reader and writer both start at 0 position)/ in-place sorting/ rarely remove method (swap)(One pointer starts at 0 position, another pointer starts at the last position)
