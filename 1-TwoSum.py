63931a7f64c683c425b5d1d1cb6bc7b096096a76
maxwell <max870701@gmail.com>
2022-10-03 22:44:50 -0700

    1. Two Sum

--- 1-TwoSum.py
+++ 1-TwoSum.py
@@ -1,9 +1,31 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:  
-        mapping = {}
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """  
+        # One-pass Hash Table
+        hash_map = {}
         
         for index in range(len(nums)):
             rest = target - nums[index]
-            if rest in mapping:
-                return [index, mapping[rest]]
-            mapping[nums[index]] = index
+            if rest in hash_map:
+                return [index, hash_map[rest]]
+            hash_map[nums[index]] = index
+    
+    def twoSum1(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        # Two-pass Hash Table
+        hash_map = {}
+
+        for i in range(len(nums)):
+            hash_map[nums[i]] = i
+        for i in range(len(nums)):
+            rest = target - nums[i]
+            if rest in hash_map and hash_map[rest] != i:
+                return [i, hash_map[rest]]
