46a7f8e92c0df7ffe71e975769ffe9e5812558c3
maxwell <max870701@gmail.com>
2022-10-04 15:31:51 -0700

    73. Set Matrix Zeros

---
+++ 73-SetMatrixZeroes.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        columns_0, rows_0 = set(), set()
+        row_len = len(matrix[0])
+        i = 0
+        
+        for m in matrix:
+            if 0 in set(m):
+                rows_0.add(i)
+                for j, n in enumerate(m):
+                    if n == 0:
+                        columns_0.add(j)
+            i += 1
+
+        for r, rows in enumerate(matrix):
+            if r in rows_0:
+                matrix[r] = [0 for e in range(row_len)]
+            else:
+                for s in columns_0:
+                    rows[s] = 0
+                    
+        return matrix
