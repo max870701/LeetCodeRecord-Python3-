d2c638cda9cbde5904bff822d559e745ad6e5add
maxwell <max870701@gmail.com>
2022-11-22 23:10:23 -0800

    Template of Binary Search

---
+++ Basic/BinarySearchFramework.py
@@ -0,0 +1,19 @@
+def binarySearch(nums, target):
+    """
+    nums: List[int]
+    target: int
+    """
+    left = right = 0
+    while ():
+        # Prevent overflow
+        mid = left + (right - left) / 2
+        if (nums[mid] == target):
+            pass
+        elif nums[mid] < target:
+            # left = ...
+            pass
+        elif nums[mid] > target:
+            # right = ...
+            pass
+    
+        return ()
