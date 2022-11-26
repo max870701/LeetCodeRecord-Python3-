3063d7eb697bfe4ec3e1f4aa67f8d37003497d6c
maxwell <max870701@gmail.com>
2022-11-26 00:03:24 -0800

    Update 380. Insert Delete Get Random O(1)

--- 380-InsertDeleteGetRandomO(1).py
+++ 380-InsertDeleteGetRandomO(1).py
@@ -54,10 +54,56 @@ class RandomizedSet(object):
 
 
         
-
-
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
 # param_1 = obj.insert(val)
 # param_2 = obj.remove(val)
 # param_3 = obj.getRandom()
+
+class RandomizedSet1(object):
+
+    def __init__(self):
+        self.nums = []
+        # Store the index of values in nums List
+        self.valToIndex = {}
+
+    def insert(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        # The element already exist
+        if val in self.valToIndex:
+            return False
+        # Not exist
+        # Store the index of the nums in valToIndex Hashset
+        # And append it into the nunms List
+        self.valToIndex[val] = len(self.nums)
+        self.nums.append(val)
+        return True
+        
+
+    def remove(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        # val not exist
+        if val not in self.valToIndex:
+            return False
+        # val exist
+        # Get the index of the val
+        index = self.valToIndex[val]
+        self.valToIndex[self.nums[-1]] = index
+        # Swap val and the last element in the nums list
+        self.nums[index], self.nums[-1] = self.nums[-1], self.nums[index]
+        self.nums.pop()
+        del self.valToIndex[val]
+        return True
+
+
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+        return choice(self.nums)
