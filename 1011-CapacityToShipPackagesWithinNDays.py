fd24fc139199a22ea7302327cf22a98a115660fd
maxwell <max870701@gmail.com>
2022-12-16 03:54:25 -0800

    1011. Capacity To Ship Packages Within D Days

---
+++ 1011-CapacityToShipPackagesWithinNDays.py
@@ -0,0 +1,43 @@
+class Solution(object):
+    def shipWithinDays(self, weights, days):
+        """
+        :type weights: List[int]
+        :type days: int
+        :rtype: int
+        """
+        # 左閉右開
+        # 取 list 中最大，分 len(weights) 天運完
+        left = max(weights)
+        # right 一天運完（全包）
+        right = sum(weights) + 1
+
+        while left < right:
+            mid = (right + left) / 2
+            if (self.f(weights, mid) == days):
+                right = mid
+            elif self.f(weights, mid) < days:
+                right = mid
+            elif self.f(weights, mid) > days:
+                left = mid + 1
+
+        return left
+
+
+    def f(self, weights, x):
+        '''
+        定義：運載能力為 x 時，需要 f(x) 天運完
+        f(x) 隨 x 的增加單調遞減
+        '''
+        days = 0
+        i = 0
+        l = len(weights)
+        while i < l:
+            cap = x
+            while i < l:
+                if cap < weights[i]:
+                    break
+                else:
+                    cap -= weights[i]
+                i += 1
+            days += 1
+        return days
