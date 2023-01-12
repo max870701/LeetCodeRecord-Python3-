e72b0272191d1e58cf269de7c0863c4d846e259e
maxwell <max870701@gmail.com>
2023-01-12 02:26:57 -0800

    78. Subsets

---
+++ 78-Subsets.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        self.res = []
+        self.track = []
+        self.backtrack(nums, 0)
+        return self.res
+    
+    def backtrack(self, nums, start):
+        # 前序位置，每個節點的值都是一個子集
+        self.res.append(list(self.track))
+        # 回溯算法框架
+        for i in range(start, len(nums)):
+            # 做選擇
+            self.track.append(nums[i])
+            # 通過 start 參數控制樹枝的遍歷，避免產生重複的子集
+            self.backtrack(nums, i+1)
+            # 撤銷選擇
+            self.track.pop()
