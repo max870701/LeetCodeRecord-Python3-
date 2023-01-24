fade612d17b4f0a323c48d628ff4170dd8e7ed00
maxwell <max870701@gmail.com>
2023-01-24 06:05:10 -0800

    538. Convert BST to Greater Tree

---
+++ 538-ConvertBSTtoGreaterTree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        self.sum = 0
+        self.traverse(root)
+        return root
+    
+    def traverse(self, root):
+        if root is None:
+            return
+        # 降序
+        self.traverse(root.right)
+        # 中序位置
+        self.sum += root.val
+        root.val = self.sum
+        ###############
+        self.traverse(root.left)
