b2e3efc06928ec69936cd52ee0881e9d0f55e5e8
maxwell <max870701@gmail.com>
2023-01-24 07:12:44 -0800

    700.  Search in a Binary Search Tree

---
+++ 700-SearchinaBinarySearchTree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        # base case
+        if root is None: return
+        # 搜索值大於目前節點，搜索右子樹
+        if val > root.val:
+            return self.searchBST(root.right, val)
+        # 搜索值小於目前節點，搜索左子樹
+        if val < root.val:
+            return self.searchBST(root.left, val)
+        # 找到搜索值並返回該節點構成的BST
+        return root
