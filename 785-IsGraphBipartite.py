69d7003e5b16a31706ad40ebce2b6e5ab6d4fae7
maxwell <max870701@gmail.com>
2023-02-10 23:38:07 -0800

    785. Is Graph Bipartite?

---
+++ 785-IsGraphBipartite.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def isBipartite(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: bool
+        """
+        # 記錄圖是否符合二分圖性質
+        self.judge = True
+        # 記錄圖中節點的顏色，False 和 True 代表兩種不同顏色
+        n = len(graph)
+        self.color = [False for _ in range(n)]
+        # 記錄途中節點是否被訪問過
+        self.visited = [False for _ in range(n)]
+        # 圖不一定連通，可能存在多個子圖
+        # 所以要把每個節點都做為起點進行一次遍歷
+        # 若發現任何一個子圖不是二分圖，整個圖都不算二分圖
+        for v in range(n):
+            if not self.visited[v]:
+                self.traverse(graph, v)
+
+        return self.judge
+    
+    def traverse(self, graph, v):
+        # DFS遍歷
+        # 如果確定不是二分圖，結束traverse
+        if self.judge == False: return
+        # 記錄訪問過的節點
+        self.visited[v] = True
+        # 訪問相鄰節點
+        for w in graph[v]:
+            # 沒訪問過
+            if not self.visited[w]:
+                self.color[w] = not self.color[v]
+                self.traverse(graph, w)
+            # 訪問過
+            else:
+                # 若 w 和 v 顏色相同，則不是二分圖
+                if self.color[w] == self.color[v]:
+                    self.judge = False
+                    return
