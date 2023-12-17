1df84004d7f353c65a4f680942ca8a7b37e88260
maxwell <max870701@gmail.com>
2023-12-16 16:07:50 -0800

    Update

--- 42-TrappingRainWater.py
+++ 42-TrappingRainWater.py
@@ -11,7 +11,7 @@ class Solution:
             r_max[i] = max(r_max[i+1], height[i])
 
         ans = 0
-        for i in range(n):
+        for i in range(1, n-1):
             ans += max(0, min(l_max[i], r_max[i]) - height[i])
 
         return ans
