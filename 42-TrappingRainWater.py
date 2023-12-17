48cb159bf400f1e4b16fcc7173aae3a64e45e58a
maxwell <max870701@gmail.com>
2023-12-16 16:26:47 -0800

    Update

--- 42-TrappingRainWater.py
+++ 42-TrappingRainWater.py
@@ -15,3 +15,22 @@ class Solution:
             ans += max(0, min(l_max[i], r_max[i]) - height[i])
 
         return ans
+
+
+class Solution2:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        l, r = 1, n-2
+        l_max, r_max = height[0], height[n-1]
+        ans = 0
+        while l <= r:
+            if l_max <= r_max:
+                ans += max(0, l_max - height[l])
+                l_max = max(l_max, height[l])
+                l += 1
+            else:
+                ans += max(0, r_max - height[r])
+                r_max = max(r_max, height[r])
+                r -= 1
+
+        return ans
