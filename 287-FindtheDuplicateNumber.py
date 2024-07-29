ec48cf863a091222fdcad31aa55bf3b89b4ed763
maxwell <max870701@gmail.com>
2024-07-28 19:57:18 -0700

    [NeetCode] LinkedList

--- 287-FindtheDuplicateNumber.py
+++ 287-FindtheDuplicateNumber.py
@@ -1,15 +1,23 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        if nums is None and len(nums) < 2: return -1
-        # 轉換為找入環節點
+        # Medium: https://pse.is/69srfy
+        # Time: O(n)
+        # Space: O(1)
+
+        # Step 1: Find the intersection of two pointers
         slow = nums[0]
         fast = nums[nums[0]]
+
         while slow != fast:
             slow = nums[slow]
             fast = nums[nums[fast]]
+
+        # Step 2: Find the entrance of the cycle
         # 將 fast 指針放回初始位置，並開始一次走一步
         fast = 0
+        
         while slow != fast:
             slow = nums[slow]
             fast = nums[fast]
+
         return slow
